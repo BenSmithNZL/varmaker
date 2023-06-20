b971a862c341cfc98d50d3c4e95115af85d70bab
Ben <bsmithnzl108@gmail.com>
2023-06-21 01:16:21 +1200

    Update

--- R/create_var.R
+++ R/create_var.R
@@ -1,5 +1,12 @@
-# Function
-
+#' Create a multivariate time series from a VAR
+#'
+#' @param x Some sort of input
+#'
+#' @return A character string
+#' @export
+#'
+#' @examples
+#' create_var("Hello")
 create_var <-function(x){
   print(x)
 }
---
+++ tests/testthat/test-create_var.R
@@ -0,0 +1,3 @@
+test_that("My test thing works", {
+  expect_equal(create_var("F"), "F")
+})
