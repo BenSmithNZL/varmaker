c7ec057a68d59c53cb8565ea53d8e6d48efc9b30
Ben <bsmithnzl108@gmail.com>
2023-06-21 15:13:10 +1200

    Update

--- R/create_var.R
+++ R/create_var.R
@@ -7,6 +7,40 @@
 #'
 #' @examples
 #' create_var("Hello")
-create_var <-function(x){
-  print(x)
+create_var <-function(coefficient_matrices, Sigma_a, n, seed = 56143868){
+
+  K = dim(coefficient_matrices[[1]])[1]
+  H = length(coefficient_matrices) - 1
+
+  ## Check the dimensions of the coefficient matrices
+
+  # Check if phi_0 is a column vector
+  if (dim(coefficient_matrices[[1]])[2] != 1) {
+    stop("phi_0 is not a column vector.")
+  }
+
+  # Check if they're all a K by K matrix
+  for (h in 2:(H+1)) {
+    if (isFALSE(dim(coefficient_matrices[[1]]) == K)) {
+      stop(paste0("phi_", h-1, " is not a ", K, " by ", K, ' matrix.',  sep = ""))
+    }
+  }
+
+  # Check if sigma is a K by K matrix
+  if (dim(Sigma_a)[1] != K) {
+    stop("Sigma is not a K by K matrix.")
+  }
+  if (dim(Sigma_a)[2] != K) {
+    stop("Sigma is not a K by K matrix.")
+  }
+
+  burn_ins = min(round(n/2), 500)
+
+  a = MASS::mvrnorm(n = n + burn_ins, mu = rep(0, K), Sigma_a)
+  z = matrix(0, nrow = n + burn_ins, ncol = K)
+
+  return(list(K = K,
+              H = H,
+              a = a,
+              z = z))
 }
