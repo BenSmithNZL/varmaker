97de37e19a5a07b82db6db6348c323c702fcaef8
Ben <bsmithnzl108@gmail.com>
2023-06-30 17:02:50 +1200

    Update

--- R/create_var.R
+++ R/create_var.R
@@ -1,18 +1,16 @@
 #' Create a multivariate time series from a VAR
 #'
-#' @param coefficient_matrices A list of the coefficient matrices
-#' @param Sigma_a Covariance matrix of the innovations
-#' @param n Number of periods
-#' @param seed Seed for the random generation of the innovations
+#' @param coefficient_matrices A list of the coefficient matrices. The first element of the list must be a K-length vector, all following elements must be K-by-K matrices.
+#' @param Sigma_a A K-by-K covariance matrix of the innovations.
+#' @param n An integer of the number of periods.
+#' @param seed Seed for the random generation of the innovations. Seed is equal to 56143868 by default.
 #'
-#' @return A list
+#' @return \code{create_var} returns a [list()] object.
+#'
 #' @export
 #'
 #' @examples
-#' create_var(list(matrix(c(1, 0.5, 0.25),
-#'                          nrow = 3,
-#'                          ncol = 1,
-#'                          byrow = TRUE),
+#' create_var(list(c(1, 0.5, 0.25),
 #'                 matrix(c(0.2, 0, 0.1,
 #'                         -0.3, 0, 0,
 #'                          0, 0, -0.1),
@@ -25,19 +23,29 @@
 #'                          nrow = 3,
 #'                          ncol = 3,
 #'                          byrow = TRUE)),
-#'                 diag(x = 1, nrow = 3, ncol = 3),
-#'                 1000)
+#'            matrix(c(1.25, 0, 0,
+#'                     0, 1, 0,
+#'                     0, 0, 0.5),
+#'                     nrow = 3,
+#'                     ncol = 3,
+#'                     byrow = TRUE),
+#'            1000)
 create_var <- function(coefficient_matrices, Sigma_a, n, seed = 56143868){
 
-  K = dim(coefficient_matrices[[1]])[1]
+  K = length(coefficient_matrices[[1]])
   H = length(coefficient_matrices) - 1
   set.seed(seed)
 
   ## Check the dimensions of the coefficient matrices
 
+  # Check that phi_0 is not a scalar.
+  if (length(coefficient_matrices[[1]]) == 1) {
+    stop("phi_0 is not a vector.")
+  }
+
   # Check if phi_0 is a column vector
-  if (dim(coefficient_matrices[[1]])[2] != 1) {
-    stop("phi_0 is not a column vector.")
+  if (is.vector(coefficient_matrices[[1]]) == FALSE) {
+    stop("phi_0 is not a vector.")
   }
 
   # Check if they're all a K by K matrix
@@ -52,6 +60,7 @@ create_var <- function(coefficient_matrices, Sigma_a, n, seed = 56143868){
     stop("Sigma is not a K by K matrix.")
   }
 
+  ## Calculate the VAR
   burn_ins = min(round(n/2), 500)
   a = MASS::mvrnorm(n = n + burn_ins, mu = rep(0, K), Sigma_a)
   z = matrix(0, nrow = n + burn_ins, ncol = K)
@@ -71,7 +80,9 @@ create_var <- function(coefficient_matrices, Sigma_a, n, seed = 56143868){
   }
 
   a <- utils::tail(a, n)
+  rownames(a) <- 1:n
   z <- utils::tail(z, n)
+  rownames(z) <- 1:n
 
   ## Creating the coefficient matrix beta
   beta = t(coefficient_matrices[[1]])
@@ -80,10 +91,21 @@ create_var <- function(coefficient_matrices, Sigma_a, n, seed = 56143868){
     beta = rbind(beta, coefficient_matrices[[h+1]])
   }
 
+  ## Calculating the expected value of the DGP.
+  mu = diag(x = 1, nrow = K, ncol = K)
+  for (h in 1:H) {
+    mu = mu - coefficient_matrices[[h+1]]
+  }
+  mu = solve(mu)
+  mu = mu %*% coefficient_matrices[[1]]
+
   ## Return output
   return(list(a = a,
               beta = beta,
-              K = K,
+              burn_ins = burn_ins,
               H = H,
+              K = K,
+              mu = mu,
+              n = n,
               z = z))
 }
--- tests/testthat/test-create_var.R
+++ tests/testthat/test-create_var.R
@@ -1,10 +1,7 @@
 test_that("Check that first element is a column vector", {
     expect_error(
       create_var(
-        list(matrix(c(1, 0.5, 0.25, 2),
-                    nrow = 2,
-                    ncol = 2,
-                    byrow = T),
+        list(2,
              matrix(c(0.2, 0, 0.1,
                       -0.3, 0, 0,
                       0, 0, -0.1),
@@ -19,14 +16,11 @@ test_that("Check that first element is a column vector", {
                     byrow = T)),
         diag(x = 1, nrow = 3, ncol = 3),
         1000),
-      c("phi_0 is not a column vector."))
+      c("phi_0 is not a vector."))
 })
 
 test_that("Check that all the other coefficient matricies are K by K.", {
-  expect_error(create_var(list(matrix(c(1, 0.5, 0.25),
-                                      nrow = 3,
-                                      ncol = 1,
-                                      byrow = T),
+  expect_error(create_var(list(c(1, 0.5, 0.25),
                                matrix(c(0.2, 0,
                                         -0.3, 0),
                                       nrow = 2,
